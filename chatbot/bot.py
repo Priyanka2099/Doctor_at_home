f6c2bfa002dac329b03cff2d4828324593217c22
priyanka2099 <priyankah243@gmail.com>
2020-01-30 11:12:45 +0530

    Updated

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -6,9 +6,10 @@ from sklearn import model_selection
 from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
 import warnings
+from itertools import groupby
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-
+from rake_nltk import Rake
+import collections
 training = pd.read_csv('Training.csv')
 testing  = pd.read_csv('Testing.csv')
 cols     = training.columns
@@ -16,10 +17,12 @@ cols     = cols[:-1]
 x        = training[cols]
 y        = training['prognosis']
 y1       = y
-
-reduced_data = training.groupby(training['prognosis']).max()
-
-#mapping strings to numbers
+df=pd.DataFrame(training, columns=training.columns.values)
+col_list=df.columns.tolist()
+val=input('Enter all the symptoms you have')
+r=Rake()
+a=r.extract_keywords_from_text(val)
+b=r.get_ranked_phrases()
 le = preprocessing.LabelEncoder()
 le.fit(y)
 y = le.transform(y)
@@ -27,73 +30,36 @@ y = le.transform(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 testx    = testing[cols]
-testy    = testing['prognosis']  
+testy    = testing['prognosis']
 testy    = le.transform(testy)
-
-
 clf1  = DecisionTreeClassifier()
 clf = clf1.fit(x_train,y_train)
-#print(clf.score(x_train,y_train))
-#print ("cross result========")
-#scores = cross_validation.cross_val_score(clf, x_test, y_test, cv=3)
-#print (scores)
-#print (scores.mean())
-#print(clf.score(testx,testy))
-
 importances = clf.feature_importances_
 indices = np.argsort(importances)[::-1]
 features = cols
 
-#feature_importances
-#for f in range(10):
-#    print("%d. feature %d - %s (%f)" % (f + 1, indices[f], features[indices[f]] ,importances[indices[f]]))
+def predict(b,col_list):
+    count = 0
+    max_count = 0
+    found = 0
+    if(any(x in b for x in col_list)):
+        for ind in df.index:
+            for i in b:
+                if(i in col_list and df.at[ind,i]==1):
+                    count+=1
 
-print("Please reply Yes or No for the following symptoms") 
-def print_disease(node):
-    #print(node)
-    node = node[0]
-    #print(len(node))
-    val  = node.nonzero() 
-    #print(val)
-    disease = le.inverse_transform(val[0])
-    return disease
-def tree_to_code(tree, feature_names):
-    tree_ = tree.tree_
-    #print(tree_)
-    feature_name = [
-        feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
-        for i in tree_.feature
-    ]
-    #print("def tree({}):".format(", ".join(feature_names)))
-    symptoms_present = []
-    def recurse(node, depth):
-        indent = "  " * depth
-        if tree_.feature[node] != _tree.TREE_UNDEFINED:
-            name = feature_name[node]
-            threshold = tree_.threshold[node]
-            print(name + " ?")
-            ans = input()
-            ans = ans.lower()
-            if ans == 'yes':
-                val = 1
-            else:
-                val = 0
-            if  val <= threshold:
-                recurse(tree_.children_left[node], depth + 1)
-            else:
-                symptoms_present.append(name)
-                recurse(tree_.children_right[node], depth + 1)
-        else:
-            present_disease = print_disease(tree_.value[node])
-            print( "You may have " +  present_disease )
-            red_cols = reduced_data.columns 
-            symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero()]
-            print("symptoms present  " + str(list(symptoms_present)))
-            print("symptoms given "  +  str(list(symptoms_given)) )  
-            confidence_level = (1.0*len(symptoms_present))/len(symptoms_given)
-            print("confidence level is " + str(confidence_level))
+            if(count>max_count):
+                max_count=count
+                found=ind
 
-    recurse(0, 1)
+        print("You may have "+df.at[found,'prognosis'])
 
-tree_to_code(clf,cols)
+    else:
+        print("Unpredictable")
+
+
+predict(b,col_list);
+
+
+
 
